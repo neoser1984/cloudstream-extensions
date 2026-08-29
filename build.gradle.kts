0a37169065a1cc355cc0654f24af17b36becbfc4
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 00:18:35 +0300

    Update Kotlin Gradle plugin version to 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
 }
 
