17aed7a8e4149877afbf8eb32190403ec37b1355
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 09:14:44 +0300

    Add files via upload

---
+++ WebDramaTurkey/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "Web Drama Turkey - Kore, Çin ve Japon dizi/film izleme."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=webdramaturkey2.com&sz=%size%"
+}
