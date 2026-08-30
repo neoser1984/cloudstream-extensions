078e67700062164a153ae3f37c79df08ebf75f56
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 09:53:40 +0300

    Add files via upload

---
+++ AsyaWatch/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "AsyaWatch - Kore, Çin, Japon dizi/film izleme."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=asyawatch.com&sz=%size%"
+}
