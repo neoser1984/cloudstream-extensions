6d0fe9f2e0c42fa86334139bf26c321b49c7298b
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 09:38:05 +0300

    Add files via upload

---
+++ DiziAsya/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "DiziAsya - Kore, Çin, Japon dizi/film/anime/program izleme."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=diziasya.com&sz=%size%"
+}
