0c62712c199f00ab4e59e03f4b94b1a2092b8e60
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 08:13:41 +0300

    Add files via upload

---
+++ Filmzal/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "Filmzal - Film ve dizi izleme."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=filmzal.me&sz=%size%"
+}
