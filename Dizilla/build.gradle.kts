11c7f769bea4a954dc3cbe2c8f542fe6efd057a4
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 20:30:44 +0300

    Add files via upload

---
+++ Dizilla/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("murattunc05 (kaynak kod)", "NeO")
+    language    = "tr"
+    description = "Dizilla tüm yabancı dizileri ücretsiz olarak Türkçe Dublaj ve altyazılı seçenekleri ile 1080P kalite izleyebileceğiniz yeni nesil yabancı dizi izleme siteniz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizilla.club&sz=%size%"
+}
