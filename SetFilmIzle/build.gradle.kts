279de2f118e840961dd1175ac238ce3800115763
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 20:39:35 +0300

    Add files via upload

---
+++ SetFilmIzle/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("murattunc05 (kaynak kod)", "NeO")
+    language    = "tr"
+    description = "Setfilmizle sitesinde, donma yaşamadan Türkçe dublaj ve altyazılı filmleri ile dizileri 1080p full HD kalitesinde izleyebilirsiniz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.setfilmizle.uk&sz=%size%"
+}
