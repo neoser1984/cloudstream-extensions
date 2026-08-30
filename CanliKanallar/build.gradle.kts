c11e68de53d6b8bd7c002d10391d3dd8ca156bc6
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 17:17:59 +0300

    Add files via upload

---
+++ CanliKanallar/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "Canlı Kanallar - Türkçe ulusal/haber/spor/müzik canlı TV kanalları (IPTV/M3U)."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=github.com&sz=%size%"
+}
