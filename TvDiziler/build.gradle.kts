5447d61afcff5b6b7be937dab41a79140288b90d
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 05:56:26 +0300

    Add files via upload

---
+++ TvDiziler/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "TvDiziler - Yerli diziler, Türkçe dublaj/altyazılı bölümler."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=tvdiziler.tv&sz=%size%"
+}
