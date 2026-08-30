c6c8623029b1938e6e7d1e6a8b6988ba87731d1e
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 20:40:46 +0300

    Add files via upload

---
+++ SetFilmIzle/src/main/kotlin/com/neo/setfilmizle/SetFilmIzlePlugin.kt
@@ -0,0 +1,14 @@
+package com.neo.setfilmizle
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class SetFilmIzlePlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(SetFilmIzle())
+        registerExtractorAPI(SetPlay())
+        registerExtractorAPI(FastPlay())
+    }
+}
