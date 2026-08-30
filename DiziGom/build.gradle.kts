e16bb4b05df214c925ac911ba3ca63ddda691146
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 08:36:58 +0300

    Add files via upload

---
+++ DiziGom/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "DiziGom - Dizi izleme."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizigom.biz&sz=%size%"
+}
