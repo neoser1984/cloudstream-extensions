83445a0fb0113ed9ef8a16b685be6553cca65685
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 03:40:41 +0300

    FilmMakinesi: initial plugin scaffold

---
+++ FilmMakinesi/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "FilmMakinesi - reklamsız, hızlı film izleme."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=filmmakinesi.to&sz=%size%"
+}
