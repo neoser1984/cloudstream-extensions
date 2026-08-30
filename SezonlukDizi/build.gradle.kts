4fe6c13e9f90f97e8b87091e0ebd7c9e8a8f2dc4
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 20:25:32 +0300

    Add files via upload

---
+++ SezonlukDizi/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("murattunc05 (kaynak kod)", "NeO")
+    language    = "tr"
+    description = "Güncel ve eski dizileri en iyi görüntü kalitesiyle bulabileceğiniz yabancı dizi izleme siteniz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=sezonlukdizi6.com&sz=%size%"
+}
