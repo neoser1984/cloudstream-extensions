873a9290c4fa1ccf9fc1e2573165e42fdc5f97ad
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 20:06:48 +0300

    Add files via upload

---
+++ RecTV/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("murattunc05 (kaynak kod)", "NeO")
+    language    = "tr"
+    description = "RecTv APK, Türkiye'deki en popüler Çevrimiçi Medya Akış platformlarından biridir. Filmlerin, Canlı Sporların, Web Dizilerinin ve çok daha fazlasının keyfini ücretsiz çıkarın."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "Live", "TvSeries")
+    iconUrl = "https://rectv.org.tr/wp-content/uploads/2024/11/rectv-modified.webp"
+}
