3551092a2c7000e88b5f542b56a595b22944c1f3
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 05:27:46 +0300

    Add files via upload

---
+++ DiziKorea/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "DiziKorea - Kore, Çin, Japon, Tayland, Tayvan, Filipin dizi ve filmleri Türkçe altyazılı izleme."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("AsianDrama", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizikorea3.com&sz=%size%"
+}
