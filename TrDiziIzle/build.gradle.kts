2d9b904f8e97882fa9ab3f552940f7dcf8e7bb61
neoser1984 <serkanturkoglu@gmail.com>
2026-08-30 06:37:27 +0300

    Add files via upload

---
+++ TrDiziIzle/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "TrDiziIzle - Yerli diziler, tüm bölümler tek parça."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=trdiziizle.tv&sz=%size%"
+}
