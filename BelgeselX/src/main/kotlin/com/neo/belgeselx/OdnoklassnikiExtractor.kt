5e97980cc32ecd5d54b57cdad705f9f177678e4d
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:17:32 +0300

    fix: OdnoklassnikiExtractor - newExtractorLink kullan

--- BelgeselX/src/main/kotlin/com/neo/belgeselx/OdnoklassnikiExtractor.kt
+++ BelgeselX/src/main/kotlin/com/neo/belgeselx/OdnoklassnikiExtractor.kt
@@ -40,14 +40,13 @@ open class Odnoklassniki : ExtractorApi() {
                 .replace("ULTRA",  "4k")
 
             callback.invoke(
-                ExtractorLink(
+                newExtractorLink(
                     source  = this.name,
                     name    = this.name,
                     url     = videoUrl,
                     referer = url,
                     quality = getQualityFromName(quality),
                     headers = userAgent,
-                    isM3u8  = false
                 )
             )
         }
