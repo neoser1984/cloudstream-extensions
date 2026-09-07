e85f6c245afc575a9c472f1107f22c987c1776b9
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 22:59:42 +0300

    feat: KultFilmler eklentisi eklendi (Kekik portu)

---
+++ KultFilmler/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("keyiflerolsun", "NeO (port)")
+    language    = "tr"
+    description = "Kült Filmler özenle en iyi filmleri derler ve iyi bir altyazılı film izleme deneyimi sunmayı amaçlar"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=kultfilmler.com&sz=%size%"
+}
