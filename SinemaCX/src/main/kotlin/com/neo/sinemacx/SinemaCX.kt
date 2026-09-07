2465b1eec222e924b241a5c44708e234809abba8
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:45:02 +0300

    fix: SinemaCX - newExtractorLink (toplu düzeltme)

--- SinemaCX/src/main/kotlin/com/neo/sinemacx/SinemaCX.kt
+++ SinemaCX/src/main/kotlin/com/neo/sinemacx/SinemaCX.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.fasterxml.jackson.annotation.JsonProperty
 
@@ -123,14 +124,15 @@ class SinemaCX : MainAPI() {
             ).parsedSafe<Panel>()?.securedLink ?: return false
 
             callback.invoke(
-                ExtractorLink(
-                    source  = this.name,
-                    name    = this.name,
-                    url     = vidUrl,
-                    referer = iframe,
-                    quality = Qualities.Unknown.value,
-                    isM3u8  = true
-                )
+                newExtractorLink(
+                    source = this.name,
+                    name   = this.name,
+                    url    = vidUrl,
+                    type   = ExtractorLinkType.M3U8
+                ) {
+                    this.referer  = iframe
+                    this.quality  = Qualities.Unknown.value
+                }
             )
         } else {
             loadExtractor(iframe, "${mainUrl}/", subtitleCallback, callback)
