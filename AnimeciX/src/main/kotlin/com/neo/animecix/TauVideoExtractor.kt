acff13ad65d63134d1ff9e9f0f92d885264cd463
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:26:53 +0300

    fix: TauVideoExtractor - newExtractorLink builder pattern

--- AnimeciX/src/main/kotlin/com/neo/animecix/TauVideoExtractor.kt
+++ AnimeciX/src/main/kotlin/com/neo/animecix/TauVideoExtractor.kt
@@ -23,13 +23,14 @@ open class TauVideo : ExtractorApi() {
         for (video in api.urls) {
             callback.invoke(
                 newExtractorLink(
-                    source  = this.name,
-                    name    = this.name,
-                    url     = video.url,
-                    referer = extRef,
-                    quality = getQualityFromName(video.label),
-                    type    = INFER_TYPE
-                )
+                    source = this.name,
+                    name   = this.name,
+                    url    = video.url,
+                    type   = INFER_TYPE
+                ) {
+                    this.referer  = extRef
+                    this.quality  = getQualityFromName(video.label)
+                }
             )
         }
     }
