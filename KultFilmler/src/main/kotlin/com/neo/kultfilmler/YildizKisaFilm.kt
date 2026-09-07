8e8a6c51a1c7d21c42c029974a4df262abec4ae8
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:57:08 +0300

    fix: KultFilmler - rating/import düzelt (toplu)

--- KultFilmler/src/main/kotlin/com/neo/kultfilmler/YildizKisaFilm.kt
+++ KultFilmler/src/main/kotlin/com/neo/kultfilmler/YildizKisaFilm.kt
@@ -5,6 +5,7 @@ package com.neo.kultfilmler
 import android.util.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.fasterxml.jackson.annotation.JsonProperty
 
 open class YildizKisaFilm : ExtractorApi() {
