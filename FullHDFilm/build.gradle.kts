c065e6d9f9fa3140e1fe6acbaabe8a128522d693
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 22:59:09 +0300

    feat: FullHDFilm eklentisi eklendi (Kekik portu)

---
+++ FullHDFilm/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("keyiflerolsun", "NeO (port)")
+    language    = "tr"
+    description = "Fullhdfilm ile en yeni vizyon filmler Full HD ve kesintisiz film sizlerle. Özgün film arşivimizle en üstün kaliteli film izle keyfini sunuyoruz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=fullhdfilm.pro&sz=%size%"
+}
