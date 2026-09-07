00dc51ba5dcda449ff4bf6f7489501f6c2e5ca90
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:57:27 +0300

    fix: Watch2Movies - rating/import düzelt (toplu)

--- Watch2Movies/src/main/kotlin/com/neo/watch2movies/W2MExtractor.kt
+++ Watch2Movies/src/main/kotlin/com/neo/watch2movies/W2MExtractor.kt
@@ -12,6 +12,7 @@ import android.webkit.WebViewClient
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
