11d597170b1cd5227020c88cab47a5ae40e72f7a
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 22:59:48 +0300

    feat: SetFilmIzle eklentisi eklendi (Kekik portu)

---
+++ SetFilmIzle/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("keyiflerolsun", "NeO (port)")
+    language    = "tr"
+    description = "Setfilmizle sitemizde, donma yaşamadan Türkçe dublaj ve altyazılı filmleri ile dizileri muhteşem 1080p full HD kalitesinde izleyebilirsiniz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.setfilmizle.de&sz=%size%"
+}
