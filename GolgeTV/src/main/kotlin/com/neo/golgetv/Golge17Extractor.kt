0b068bafd6f2ea9efbd6a72269b34c02adbad9e8
neoser1984 <serkanturkoglu@gmail.com>
2026-09-08 00:13:02 +0300

    fix: GolgeTV - newExtractorLink import eklendi

--- GolgeTV/src/main/kotlin/com/neo/golgetv/Golge17Extractor.kt
+++ GolgeTV/src/main/kotlin/com/neo/golgetv/Golge17Extractor.kt
@@ -9,6 +9,7 @@ import com.lagradost.cloudstream3.utils.AppUtils
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
