fe4751442228c22d92e59fdd285618771e77ab98
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 22:59:41 +0300

    feat: KoreanTurk eklentisi eklendi (Kekik portu)

---
+++ KoreanTurk/src/main/kotlin/com/neo/koreanturk/KoreanTurkPlugin.kt
@@ -0,0 +1,12 @@
+package com.neo.koreanturk
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class KoreanTurkPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(KoreanTurk())
+    }
+}
