165c9967abd3f81ee94c86625727474cfca8e9cc
neoser1984 <serkanturkoglu@gmail.com>
2026-09-08 00:21:41 +0300

    fix: GolgeTV - ExtractorLinkType import eklendi

--- GolgeTV/src/main/kotlin/com/neo/golgetv/GolgeTV.kt
+++ GolgeTV/src/main/kotlin/com/neo/golgetv/GolgeTV.kt
@@ -18,6 +18,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
 
 class GolgeTV : MainAPI() {
     override var name = "GolgeTV"
