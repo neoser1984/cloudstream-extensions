1a0e82a9279a858cf668ed4b2c74698e04507656
neoser1984 <serkanturkoglu@gmail.com>
2026-09-08 00:13:01 +0300

    fix: GolgeTV - newExtractorLink import eklendi

--- GolgeTV/src/main/kotlin/com/neo/golgetv/Golge16Extractor.kt
+++ GolgeTV/src/main/kotlin/com/neo/golgetv/Golge16Extractor.kt
@@ -8,6 +8,7 @@ import com.lagradost.cloudstream3.utils.AppUtils
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.newExtractorLink
 
 open class Golge16 : ExtractorApi() {
     override val name = "Golge16"
