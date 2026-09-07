623d7bb44906b8e4cfa71c931acde10a191b8bec
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 22:59:31 +0300

    feat: IzleAI eklentisi eklendi (Kekik portu)

---
+++ IzleAI/src/main/kotlin/com/neo/izleai/IzleAIPlugin.kt
@@ -0,0 +1,12 @@
+package com.neo.izleai
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class IzleAIPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(IzleAI())
+    }
+}
