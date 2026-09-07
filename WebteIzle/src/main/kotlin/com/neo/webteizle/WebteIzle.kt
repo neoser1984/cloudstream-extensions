9a4af6dd6427fa50195fb548a2dcaa429ee0c78b
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:57:30 +0300

    fix: WebteIzle - rating/import düzelt (toplu)

--- WebteIzle/src/main/kotlin/com/neo/webteizle/WebteIzle.kt
+++ WebteIzle/src/main/kotlin/com/neo/webteizle/WebteIzle.kt
@@ -127,7 +127,6 @@ class WebteIzle : MainAPI() {
         val year        = document.selectXpath("//td[contains(text(), 'Vizyon')]/following-sibling::td").text().trim().split(" ").last().toIntOrNull()
         val description = document.selectFirst("blockquote")?.text()?.trim()
         val tags        = document.selectXpath("//a[@itemgroup='genre']").map { it.text() }
-        val rating      = document.selectFirst("div.detail")?.text()?.trim()?.replace(",", ".").toRatingInt()
         val duration    = document.selectXpath("//td[contains(text(), 'Süre')]/following-sibling::td").text().trim().split(" ").first().toIntOrNull()
         val trailer     = document.selectFirst("button#fragman")?.attr("data-ytid")
         val actors      = document.selectXpath("//div[@data-tab='oyuncular']//a").map {
@@ -139,7 +138,6 @@ class WebteIzle : MainAPI() {
             this.year      = year
             this.plot      = description
             this.tags      = tags
-            this.rating    = rating
             this.duration  = duration
             addTrailer("https://www.youtube.com/embed/${trailer}")
             addActors(actors)
