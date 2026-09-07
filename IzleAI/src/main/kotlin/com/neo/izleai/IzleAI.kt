51490a8862f6d78e9ddb1ebf4a7b1ae44cd0a0b4
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:57:02 +0300

    fix: IzleAI - rating/import düzelt (toplu)

--- IzleAI/src/main/kotlin/com/neo/izleai/IzleAI.kt
+++ IzleAI/src/main/kotlin/com/neo/izleai/IzleAI.kt
@@ -121,7 +121,6 @@ class IzleAI : MainAPI() {
         val year        = document.selectFirst("a[href*='/yil/']")?.text()?.toIntOrNull()
         val description = document.selectFirst("div.mv-det-p")?.text()?.trim() ?: document.selectFirst("div.w-full div.text-base")?.text()?.trim()
         val tags        = document.select("[href*='kategori']").map { it.text() }
-        val rating      = document.selectFirst("a[href*='imdb.com'] span.font-bold")?.text()?.trim().toRatingInt()
         val duration    = document.selectXpath("//span[contains(text(), ' dk.')]").text().trim().split(" ").first().toIntOrNull()
         val trailer     = document.selectFirst("iframe[data-src*='youtube.com/embed/']")?.attr("data-src")
         val actors      = document.select("div.flex.overflow-auto [href*='oyuncu']").map {
@@ -133,7 +132,6 @@ class IzleAI : MainAPI() {
             this.year      = year
             this.plot      = description
             this.tags      = tags
-            this.rating    = rating
             this.duration  = duration
             addTrailer(trailer)
             addActors(actors)
