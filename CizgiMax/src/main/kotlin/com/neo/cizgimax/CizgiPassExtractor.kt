73f7ae25519a8f8cf43fbdf8cf27fa346084d511
neoser1984 <serkanturkoglu@gmail.com>
2026-09-08 00:28:29 +0300

    chore: CizgiMax silindi (derleme sorunları)

--- CizgiMax/src/main/kotlin/com/neo/cizgimax/CizgiPassExtractor.kt
+++
@@ -1,8 +0,0 @@
-// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
-
-package com.neo.cizgimax
-
-class CizgiPass : CizgiDuo() {
-    override var name    = "CizgiPass"
-    override var mainUrl = "https://cizgipass5.online"
-}
