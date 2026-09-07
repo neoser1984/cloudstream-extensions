2ee799ac5add6f7602833631cf8654a0219b7764
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 22:58:58 +0300

    feat: DiziPal eklentisi eklendi (Kekik portu)

---
+++ DiziPal/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("keyiflerolsun", "muratcesmecioglu", "NeO (port)")
+    language    = "tr"
+    description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://dizipal683.com&sz=%size%"
+}
