55ce4d9503ec871ac9f672930f72f4e0e4b82a08
neoser1984 <serkanturkoglu@gmail.com>
2026-09-07 23:45:08 +0300

    fix: UgurFilm - newExtractorLink (toplu düzeltme)

--- UgurFilm/src/main/kotlin/com/neo/ugurfilm/OdnoklassnikiExtractor.kt
+++ UgurFilm/src/main/kotlin/com/neo/ugurfilm/OdnoklassnikiExtractor.kt
@@ -40,15 +40,16 @@ open class Odnoklassniki : ExtractorApi() {
                 .replace("ULTRA",  "4k")
 
             callback.invoke(
-                ExtractorLink(
-                    source  = this.name,
-                    name    = this.name,
-                    url     = videoUrl,
-                    referer = url,
-                    quality = getQualityFromName(quality),
-                    headers = userAgent,
-                    isM3u8  = false
-                )
+                newExtractorLink(
+                    source = this.name,
+                    name   = this.name,
+                    url    = videoUrl,
+                    type   = INFER_TYPE
+                ) {
+                    this.referer  = url
+                    this.quality  = getQualityFromName(quality)
+                    this.headers  = userAgent
+                }
             )
         }
     }
