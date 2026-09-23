36a0cc460b2d330e7ac4dea7ecf8e5322daff3ac
neoser1984 <serkanturkoglu@gmail.com>
2026-09-23 22:46:40 +0300

    feat: DiziPalOrjinal eklentisi (dizipalorjinal9.com)

---
+++ DiziPalOrjinal/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "DiziPal Orjinal - Türkçe dublaj ve altyazılı filmler ve diziler."
+
+    status  = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizipalorjinal9.com&sz=%size%"
+}
