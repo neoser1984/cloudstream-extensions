66fc38406d4e76d343a429124899cba27734b822
neoser1984 <serkanturkoglu@gmail.com>
2026-09-23 22:06:40 +0300

    feat: DizizOn eklentisi eklendi

---
+++ DizizOn/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("NeO")
+    language    = "tr"
+    description = "DizizOn - Yabancı dizileri online full HD kalitesinde izleyin."
+
+    status  = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.dizizon.com&sz=%size%"
+}
